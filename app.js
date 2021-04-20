74197906e3602d6d4bbac5f44e6bb3ba6d084309
JER.8789 <je.rogers8789@gmail.com>
2021-04-19 21:09:37 -0500

    app still not running correctly

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ function runApp (){
             }
 })};
 function viewAllEmp() {
-    connection.query('SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;', 
+    connection.query('SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;', 
     function(err, res) {
       if (err) throw err
       console.table(res)
@@ -230,4 +230,3 @@ function addDept() {
             }
         )
 })};
-runApp();
--- assets/empData_DB.sql
+++ assets/empData_DB.sql
@@ -1,26 +1,68 @@
-Drop DATABASE if EXISTS empData_DB
+DROP DATABASE IF EXISTS empData_DB;
+
 CREATE DATABASE empData_DB;
+
 USE empData_DB;
 
-CREATE TABLE departments(
-id INT AUTO_INCREMENT PRIMARY KEY,
-dept_name VARCHAR(30)
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+ 
+);
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
-CREATE TABLE roles (
-id INT AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(30),
-salary DECIMAL(8,2),
-dept_id INT,
-FOREIGN KEY(dept_id) REFERENCES departments(id)
-);
+INSERT INTO department (name)
+VALUE ('Communications');
+INSERT INTO department (name)
+VALUE ('IT');
+INSERT INTO department (name)
+VALUE ('Finance');
+INSERT INTO department (name)
+VALUE ('Sales');
 
-CREATE TABLE employees(
-id INT AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INT,
-manager_id INT,
-FOREIGN KEY(role_id) REFERENCES roles(id),
-FOREIGN KEY(manager_id) REFERENCES employees(id)
-);
+INSERT INTO role (title, salary, department_id)
+VALUE ('DB Administrator', 90000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Sales Director', 850000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 55000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Marketing Director', 70000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Salesperson', 65000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Software Engineer', 95000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Financial Analyst', 250000, 3);
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ('Bugs', 'Bunny', null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ('Donald', 'Duck', null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ('Sylvester','Slick', null, 3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ('Tweety','Bird', 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ('Tom','Cat', 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ('Jerry','Mouse', 2, 7);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
